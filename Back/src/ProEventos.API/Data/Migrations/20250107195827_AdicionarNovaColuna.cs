33f03430a8644ac896c8b1535d60688667739cfc
WanessaMara <wanessamaralima15@gmail.com>
2025-01-07 17:12:27 -0300

    Teste de Migrations (add e remove).

---
+++ Back/src/ProEventos.API/Data/Migrations/20250107195827_AdicionarNovaColuna.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class AdicionarNovaColuna : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Teste",
+                table: "Eventos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Teste",
+                table: "Eventos");
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/Data/Migrations/20250107200218_RemoverNovaColuna.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class RemoverNovaColuna : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Teste",
+                table: "Eventos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+    }
+}
