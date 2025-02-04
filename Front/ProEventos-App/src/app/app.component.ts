06078278a8106cc2a5888be04c9c4bae9e42c81a
WanessaMara69 <wanessamaralima15@gmail.com>
2025-02-04 16:55:08 -0300

    Edições realizadas.

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { EventosComponent } from './eventos/eventos.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NavegacaoComponent } from "./navegacao/navegacao.component";
@@ -7,12 +7,14 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { EventoService } from './services/evento.service';
-import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
-import  {  NgxSpinnerModule  }  from  'ngx-spinner' ;
-import { ToastrModule } from 'ngx-toastr';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
 
 @Component({
   selector: 'app-root',
@@ -22,14 +24,17 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule, 
     NavegacaoComponent,
     CollapseModule,
-    EventosComponent, 
+    EventosComponent,
+    PalestrantesComponent,
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent, 
+    TituloComponent,
     FormsModule, 
     RouterModule, 
     TooltipModule,
     BsDropdownModule,
-    ModalModule,
-    NgxSpinnerModule,
-    ToastrModule,
+    ModalModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -3,20 +3,21 @@ import { CommonModule } from '@angular/common';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { Component, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { EventoService } from '../services/evento.service';
-import { Evento } from '../models/Evento';
-import { DateTimeFormatPipe } from "../helpers/DateTimeFormat.pipe";
+import { EventoService } from '../../services/evento.service';
+import { Evento } from '../../models/Evento';
+import { DateTimeFormatPipe } from "../../helpers/DateTimeFormat.pipe";
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { TituloComponent } from "../../shared/titulo/titulo.component";
 
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [CommonModule, CollapseModule, HttpClientModule, FormsModule, DateTimeFormatPipe, TooltipModule, 
-    BsDropdownModule],
+  imports: [CommonModule, CollapseModule, HttpClientModule, FormsModule, DateTimeFormatPipe, TooltipModule,
+    BsDropdownModule, TituloComponent],
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss'],
   encapsulation: ViewEncapsulation.None
